5932c9a56c9500ac9b81729d43397b7b891ecf54
AhmedGamal-Gemy <ahmed.gamal.482004@gmail.com>
2024-07-05 15:48:13 +0300

    Remove deleted files from repository

--- Source-code/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module idk.source.code {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens idk.source.code to javafx.fxml;
-    exports idk.source.code;
-}
